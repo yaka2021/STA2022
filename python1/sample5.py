5e6b01d18921d6979d22a2c56317cd869e519cbd
yaka2021 <96155487+yaka2021@users.noreply.github.com>
2022-04-27 15:28:19 +0900

    Update sample5.py

--- python1/sample5.py
+++ python1/sample5.py
@@ -1,9 +1,11 @@
 a = 3127
 b = 8201
 
-i = a
-
-while a % i != 0 or b % i != 0:
-      i = i - 1
+while a != b:
     
-print(i);
+      if a > b:
+         a = a - b
+      else:
+         b = b - a
+    
+print(a);
